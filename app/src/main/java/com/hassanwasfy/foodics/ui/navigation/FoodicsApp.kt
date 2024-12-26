71e255889f491e61ab99456583d01789403a167d
hassanwasfy <hassanwasfy7@gmail.com>
2024-12-26 23:41:11 +0200

    [SETUP] - prepare the basics

--- app/src/main/java/com/hassanwasfy/foodics/ui/navigation/FoodicsApp.kt
+++ app/src/main/java/com/hassanwasfy/foodics/ui/navigation/FoodicsApp.kt
@@ -2,20 +2,16 @@ package com.hassanwasfy.foodics.ui.navigation
 
 
 import android.annotation.SuppressLint
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.navigation.compose.rememberNavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MoneyEyeApp() {
+fun FoodicsApp(innerPadding: PaddingValues) {
     val navController = rememberNavController()
     CompositionLocalProvider(LocalNavController provides navController) {
-        Scaffold {
-            MoneyEyeNavGraph()
-        }
+        FoodicsGraph(innerPadding)
     }
 }
