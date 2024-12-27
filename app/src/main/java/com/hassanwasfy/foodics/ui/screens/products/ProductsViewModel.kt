31ffbe4b1746c712a0c098303fd4c84cc6177e87
hassanwasfy <hassanwasfy7@gmail.com>
2024-12-27 17:08:23 +0200

    [SETUP] - init ProductsViewModel

---
+++ app/src/main/java/com/hassanwasfy/foodics/ui/screens/products/ProductsViewModel.kt
@@ -0,0 +1,7 @@
+package com.hassanwasfy.foodics.ui.screens.products
+
+import com.hassanwasfy.foodics.data.repository.ProductsRepository
+
+class ProductsViewModel(private val repository: ProductsRepository) {
+
+}
