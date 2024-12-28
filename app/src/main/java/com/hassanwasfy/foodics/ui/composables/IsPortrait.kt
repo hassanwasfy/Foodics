a8f60522d64f9fd5515ea3441d226395a6c95b75
hassanwasfy <hassanwasfy7@gmail.com>
2024-12-28 13:31:39 +0200

    [dev/presentation-layer]
    - implement a heper function for a better responsive ui

---
+++ app/src/main/java/com/hassanwasfy/foodics/ui/composables/IsPortrait.kt
@@ -0,0 +1,11 @@
+package com.hassanwasfy.foodics.ui.composables
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+
+@Composable
+fun isPortrait(): Boolean {
+    val configuration = LocalConfiguration.current
+    return configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+}
