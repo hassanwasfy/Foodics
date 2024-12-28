31a889e71a50d1d02c51105bf565c4592efcf907
hassanwasfy <hassanwasfy7@gmail.com>
2024-12-28 13:32:43 +0200

    [dev/presentation-layer]
    - init placeholder screens

--- app/src/main/java/com/hassanwasfy/foodics/ui/screens/menu/Screen.kt
+++ app/src/main/java/com/hassanwasfy/foodics/ui/screens/menu/Screen.kt
@@ -1,9 +1,12 @@
 package com.hassanwasfy.foodics.ui.screens.menu
 
 import androidx.compose.runtime.Composable
+import com.hassanwasfy.foodics.ui.composables.ScreenBase
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ScreenMenu(){
-
+    ScreenBase(
+        isLoading = true, isError = false, errorMsg = ""
+    ) {}
 }
--- app/src/main/java/com/hassanwasfy/foodics/ui/screens/settings/Screen.kt
+++ app/src/main/java/com/hassanwasfy/foodics/ui/screens/settings/Screen.kt
@@ -1,9 +1,12 @@
 package com.hassanwasfy.foodics.ui.screens.settings
 
 import androidx.compose.runtime.Composable
+import com.hassanwasfy.foodics.ui.composables.ScreenBase
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun ScreenSettings(){
-
+fun ScreenSettings() {
+    ScreenBase(
+        isLoading = true, isError = false, errorMsg = ""
+    ) {}
 }
