976873e354e04a7b6867181651cc3ee5016cd960
hassanwasfy <hassanwasfy7@gmail.com>
2024-12-28 13:32:55 +0200

    [dev/presentation-layer]
    - add more colors

--- app/src/main/java/com/hassanwasfy/foodics/ui/theme/Color.kt
+++ app/src/main/java/com/hassanwasfy/foodics/ui/theme/Color.kt
@@ -12,7 +12,7 @@ val Pink40 = Color(0xABE3BDFF)
 
 
 
-val Primary_Color = Color(0xFF590098)
+val Primary_Color = Color(0xFF470098)
 val Red_Circle = Color(0xFFE80000)
 val Green_Circle = Color(0xFF65943B)
 val Gray_Background = Color(0xFFE8E7EB)
